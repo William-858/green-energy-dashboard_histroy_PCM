ff7ddeaaf164d5c37b3f94534751b6e3b31dc450
William-858 <w00556075@triwra.org.tw>
2026-07-16 21:11:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = 'green-dashboard-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './data.json',
+  './opinions.json'
+];
 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
